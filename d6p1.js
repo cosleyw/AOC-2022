06b360b9e086b5c9da1000023f139cd1b68706be
cosleyw <willcosley@gmail.com>
2023-03-21 10:29:50 -0500

    fixed an offset

--- d6p1.js
+++ d6p1.js
@@ -3,7 +3,7 @@ let input = prompt("problem input");
 for(let i = 0; i < input.length; i++){
 	let s = new Set(input.slice(i, 4));
 	if(s.size == 4){
-		confirm(i);
+		confirm(i + 4);
 		break;
 	}
 }
