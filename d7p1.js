73dd1bb7fe6ac443b41d310364986538b18efc95
cosleyw <willcosley@gmail.com>
2023-03-21 11:41:20 -0500

    completed day 7 part 1

---
+++ d7p1.js
@@ -0,0 +1,67 @@
+let input = prompt("problem input");
+input = input.split(/\n+/g)
+	.map(v=>v.split(/ +/g));
+
+let i = 0;
+
+let rfs = {};
+
+function parse(fs){
+	for(; i < input.length;){
+		switch(input[i][0]){
+			case "$":
+				switch(input[i][1]){
+					case "cd":
+						switch(input[i][2]){
+							case "..":
+								i++;
+								return;
+								break;
+							case "/":
+								i++;
+								parse(rfs);
+								break;
+							default:
+								fs[input[i][2]] ??= {};
+								parse(fs[input[i++][2]]);
+						}
+						break;
+					case "ls":
+						i++;
+						break;
+				}
+				break;
+			case "dir":
+				fs[input[i][1]] ??= {};
+				i++;
+				break;
+			default:
+				fs[input[i][1]] = input[i][0];
+				i++;
+		}
+	}
+}
+
+while(i < input.length)
+	parse(rfs);
+
+let sd = {};
+function sizes(fs, p){
+	let sum = 0;
+	for(f in fs){
+		if(typeof(fs[f]) == typeof(""))
+			sum += +fs[f];
+		else
+			sum += sizes(fs[f], p + "/" + f);
+	}
+	sd[p] = sum;
+	return sum;
+}
+
+let sum = 0;
+for(f in sd){
+	if(sd[f] <= 100000)
+		sum += sd[f];
+}
+
+confirm(sum);
