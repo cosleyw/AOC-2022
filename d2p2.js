bc096f653641a388958340df0957e41d8a6ad18b
cosleyw <willcosley@gmail.com>
2023-03-20 20:26:40 -0500

    completed day 2

---
+++ d2p2.js
@@ -0,0 +1,14 @@
+let input = prompt("problem input");
+
+function scoreRound(a, b){
+		let scoreDict = {"X": 1, "Y": 2, "Z": 3, "AX": 3, "AY": 6, "AZ": 0, "BX": 0, "BY": 3, "BZ": 6, "CX": 6, "CY": 0, "CZ": 3};
+
+		return scoreDict[b] + scoreDict[a + b];
+}
+
+confirm(
+	    input
+			.split(/\n+/g)
+			.map(v=>scoreRound(...v.split(/ +/)))
+			.reduce((a,b)=>a+b)
+);
