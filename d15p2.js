1883576407565cf3bcb4abbfdd3d3def21fcb955
cosleyw <willcosley@gmail.com>
2023-04-26 16:17:06 -0500

    completed day 15 part 2

---
+++ d15p2.js
@@ -0,0 +1,46 @@
+let input = prompt("problem input").split(/\n/g).map(v=>v.match(/(-|)[0-9]+/g).map(v=>+v));
+
+input = input.map(([a,b,c,d])=>[a,b,Math.abs(a-c) + Math.abs(b-d)]);
+
+function inRange(a, b, c){
+	return c >= a && c <= b;
+}
+
+function count(ln){
+	let sections = [];
+	for(let i = 0; i < input.length; i++){
+		let cur = input[i];
+		let l = cur[2]-Math.abs(cur[1] - ln);
+		if(l > 0)
+			sections.push([cur[0] - l , cur[0] + l]);
+	}
+	sections = sections.sort((a,b)=>a[0]-b[0]);
+	let s2 = [sections[0]];
+	for(let i = 1; i < sections.length; i++){
+		let s = s2[s2.length-1];
+		if(inRange(s[0], s[1], sections[i][0])){
+			s[1] = Math.max(s[1], sections[i][1]);
+		}else{
+			s2.push(sections[i]);
+		}
+	}
+
+	let sum = 0;
+	for(let i = 0; i < s2.length; i++){
+		let p1 = 0, p2 = 4000000;
+		if(inRange(0, 4000000, s2[i][0])){
+			p1 = s2[i][0];
+		}
+		if(inRange(0, 4000000, s2[i][1])){
+			p2 = s2[i][1];
+		}
+		sum += p2 - p1;
+	}
+	return [sum, s2];
+}
+
+for(let i = 0; i <= 4000000; i++){
+	let res = count(i);
+	if(res[0] != 4000000)
+		console.log(i, res);
+}
